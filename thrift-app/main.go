e81a1f8ad180a219e50239b93ec6f49a766e31fb
Tong Cai <caitong93@gmail.com>
2020-09-08 15:39:54 +0800

    Add thrift app

---
+++ thrift-app/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"sample/gen-go/Sample"
+
+	"github.com/apache/thrift/lib/go/thrift"
+)
+
+func Usage() {
+	fmt.Fprint(os.Stderr, "Usage of ", os.Args[0], ":\n")
+	flag.PrintDefaults()
+	fmt.Fprint(os.Stderr, "\n")
+}
+
+type Greeter struct {
+}
+
+func (this *Greeter) SayHello(ctx context.Context, u *Sample.User) (r *Sample.Response, err error) {
+	strJson, _ := json.Marshal(u)
+	return &Sample.Response{ErrCode: 0, ErrMsg: "success", Data: map[string]string{"User": string(strJson)}}, nil
+}
+
+func (this *Greeter) GetUser(ctx context.Context, uid int32) (r *Sample.Response, err error) {
+	return &Sample.Response{ErrCode: 1, ErrMsg: "user not exist."}, nil
+}
+
+func main() {
+	flag.Usage = Usage
+	protocol := flag.String("P", "binary", "Specify the protocol (binary, compact, json, simplejson)")
+	framed := flag.Bool("framed", false, "Use framed transport")
+	buffered := flag.Bool("buffered", false, "Use buffered transport")
+	addr := flag.String("addr", "localhost:9090", "Address to listen to")
+
+	flag.Parse()
+
+	//protocol
+	var protocolFactory thrift.TProtocolFactory
+	switch *protocol {
+	case "compact":
+		protocolFactory = thrift.NewTCompactProtocolFactory()
+	case "simplejson":
+		protocolFactory = thrift.NewTSimpleJSONProtocolFactory()
+	case "json":
+		protocolFactory = thrift.NewTJSONProtocolFactory()
+	case "binary", "":
+		protocolFactory = thrift.NewTBinaryProtocolFactoryDefault()
+	default:
+		fmt.Fprint(os.Stderr, "Invalid protocol specified", protocol, "\n")
+		Usage()
+		os.Exit(1)
+	}
+
+	//buffered
+	var transportFactory thrift.TTransportFactory
+	if *buffered {
+		transportFactory = thrift.NewTBufferedTransportFactory(8192)
+	} else {
+		transportFactory = thrift.NewTTransportFactory()
+	}
+
+	//framed
+	if *framed {
+		transportFactory = thrift.NewTFramedTransportFactory(transportFactory)
+	}
+
+	//handler
+	handler := &Greeter{}
+
+	//transport,no secure
+	var err error
+	var transport thrift.TServerTransport
+	transport, err = thrift.NewTServerSocket(*addr)
+	if err != nil {
+		fmt.Println("error running server:", err)
+	}
+
+	//processor
+	processor := Sample.NewGreeterProcessor(handler)
+
+	fmt.Println("Starting the simple server... on ", *addr)
+
+	//start tcp server
+	server := thrift.NewTSimpleServer4(processor, transport, transportFactory, protocolFactory)
+	err = server.Serve()
+
+	if err != nil {
+		fmt.Println("error running server:", err)
+	}
+}
---
+++ thrift-app/server.thrift
@@ -0,0 +1,21 @@
+include "User.thrift"
+
+namespace go Sample
+
+typedef map<string, string> Data
+
+struct Response {
+    1:required i32 errCode;
+    2:required string errMsg;
+    3:required Data data;
+}
+
+service Greeter {
+    Response SayHello(
+        1:required User.User user
+    )
+
+    Response GetUser(
+        1:required i32 uid
+    )
+}
